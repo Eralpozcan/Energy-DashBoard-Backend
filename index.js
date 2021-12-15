957e3c107ad6135f949958dccfd280025bd261a9
Eralp <eralpozcans@gmail.com>
2021-12-15 20:58:57 +0300

    :sparkles: feat: create request adress,get,del,post,put for factorylist and factorydata

--- index.js
+++ index.js
@@ -4,12 +4,25 @@ const dotenv = require('dotenv')
 const mongoose = require('mongoose')
 
 
+const db = require('./db/index')
+const account = require('./routers/userRouters')
+
 const app = express()
 
 app.use(cors())
+app.use('/account',account)
 app.use(express.urlencoded({extended:true}))
 dotenv.config()
 
+app.get('/factorylist',db.dashboard.getFactoryList)
+app.delete('/factorylist',db.dashboard.delFactoryList)
+app.post('/factorylist',db.dashboard.createFactoryList)
+app.put('/factorylist',db.dashboard.updateFactoryList)
+
+app.get('/factorydata',db.dashboard.getFactoryData)
+app.delete('/factorydata',db.dashboard.delFactoryData)
+app.post('/factorydata',db.dashboard.createFactoryData)
+app.put('/factorydata',db.dashboard.updateFactoryData)
 
 
 mongoose.connect(process.env.DATABASE,{
