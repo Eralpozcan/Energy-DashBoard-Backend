56971136dea69a5e9db7ff4fc03e3acac9a0c7cf
Eralp <eralpozcans@gmail.com>
2021-12-18 18:53:23 +0300

    :bug: fix: get request auth depencie removed

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const account = require('./routers/userRouters')
 const app = express()
 
 app.use(cors())
+app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 dotenv.config()
 
@@ -23,12 +24,12 @@ dotenv.config()
 app.use('/account',account)
 
 
-app.get('/factorylist',auth,db.dashboard.getFactoryList)
+app.get('/factorylist',db.dashboard.getFactoryList)
 app.delete('/factorylist',auth,isAdmin,log,db.dashboard.delFactoryList)
 app.post('/factorylist',auth,isEditor,log,db.dashboard.createFactoryList)
 app.put('/factorylist',auth,isEditor,log,db.dashboard.updateFactoryList)
 
-app.get('/factorydata',auth,db.dashboard.getFactoryData)
+app.get('/factorydata',db.dashboard.getFactoryData)
 app.delete('/factorydata',auth,isAdmin,log,db.dashboard.delFactoryData)
 app.post('/factorydata',auth,isEditor,log,db.dashboard.createFactoryData)
 app.put('/factorydata',auth,isEditor,log,db.dashboard.updateFactoryData)
